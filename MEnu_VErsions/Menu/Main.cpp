f8bcd5246faeb31fe1e08797c316a77c617dbc34
Specsaverz <63544489+Specsaverz@users.noreply.github.com>
2020-11-25 19:47:21 +0300

    Trying to figure out how cmake works

--- MEnu_VErsions/Menu/Main.cpp
+++ MEnu_VErsions/Menu/Main.cpp
@@ -5,16 +5,29 @@
 using namespace std;
 int main() {
   int input;
+  cout << "Choose the Fucntion!" << endl;
   cin >> input;
-  if (input == 1) {
-    int x1, y1, z1, d1;
-    ChangeTheJ(x1, y1, z1, d1);
-  } else if (input == 2) {
-    int x2 = 1, a2 = 1;
-    long long y2 = 1, z2 = 1, d2 = 1;
-    Fibonum(x2, y2, z2, d2, a2);
-
-  } else
+  if (input <= 3) {
+    switch (input) {
+    case 1: {
+      cout << "----------------------ChangeTheJ----------------------" << endl;
+      int x1, y1, z1, d1;
+      ChangeTheJ(x1, y1, z1, d1);
+    } 
+    break;
+    case 2: {
+      cout << "----------------------Fibonum----------------------" << endl;
+      int x2 = 1, a2 = 1;
+      long long y2 = 1, z2 = 1, d2 = 1;
+      Fibonum(x2, y2, z2, d2, a2);
+    }
+    break;
+    case 3:
+      cout << "Not available yet,sorry!"<< endl;
+      break;
+    }
+  } else {
     cout << "Something went wrong!" << endl;
-  return 0;
+    return 0;
+  }
 }
