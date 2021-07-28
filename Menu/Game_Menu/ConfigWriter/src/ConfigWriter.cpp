b49b5bd8cec231b0e075804e29ac65fd4a3f8a9a
odka <63544489+o-dka@users.noreply.github.com>
2021-07-28 15:19:46 +0300

    improved ConfigWriter

--- Menu/Game_Menu/ConfigWriter/src/ConfigWriter.cpp
+++ Menu/Game_Menu/ConfigWriter/src/ConfigWriter.cpp
@@ -8,28 +8,29 @@ void Writer(string input, string filename, string change) {
   fstream file(filename.c_str());
   getline(file, input);
   getline(file, check);
-  if (check == "1") {  // checks if the file has been changed before
+   file.close();
+  if (check == "1")   // checks if the file has been changed before
     cout << "Settings changed already, starting.."<< "\n";
-  } else {
+  else {
   INPUT:
     cout << input << endl;
     cout << "what to change?" << endl;
     cin >> choose;
     cout << "what to change to ?" << endl;
     cin >> change;
-    if ((input.find(choose) < input.length()) && (change.length() == 1))
-      input.replace(input.find(choose), 1, change);
-    if ((input.find(choose) < input.length()) && (change.length() == 2))
-      input.replace(input.find(choose), 2, change);
+    if (input.find(choose) < input.length()) 
+      switch (change.length()) {
+          case 1:
+            input.replace(input.find(choose), 1, change);
+          case 2:
+            input.replace(input.find(choose), 2, change);
+      }
     if ((input.find(choose) < input.length()) && (change.length() >2)) {  // possibly will add a few more failsafes, for now there is one
       cout << "You can't do that!" << endl << endl;
       goto INPUT;
     }
-    file.close();
-    file.open("../Configs/ConfigChanger.txt",std::ofstream::out | std::ofstream::trunc);  // deletes contents of a file, writes new ones
-    file << input << "\n";
-    check = "1";
-    file << check;
+    file.open("../Configs/ConfigChanger.txt",ofstream::out |ofstream::trunc);  // deletes contents of a file, writes new ones
+    file << input << "\n1";
     cout << "Changed to :" << endl << input;
   }
 }
