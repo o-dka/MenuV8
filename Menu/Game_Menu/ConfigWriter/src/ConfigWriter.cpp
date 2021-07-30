9faaf4dd0c2d6e84be89c88744f1d34887055595
odka <63544489+o-dka@users.noreply.github.com>
2021-07-30 19:35:56 +0300

    improved the ui of config writer

--- Menu/Game_Menu/ConfigWriter/src/ConfigWriter.cpp
+++ Menu/Game_Menu/ConfigWriter/src/ConfigWriter.cpp
@@ -10,23 +10,26 @@ void Writer(string input, string filename, string change) {
   getline(file, check);
   file.close();
   if (check == "1")   // checks if the file has been changed before
-    cout << "Settings changed already, starting.."<< "\n";
+    cout << "Settings changed already, starting..\n";
   else {
   INPUT:
-    cout << input << endl;
-    cout << "what to change?" << endl;
+    cout << "\n" <<input << endl;
+    cout << "what to change?\n";
     cin >> choose;
-    cout << "what to change to ?" << endl;
+    cout << "what to change to ?\n";
     cin >> change;
     if (input.find(choose) < input.length()) 
       switch (change.length()) {
         case 1:
           input.replace(input.find(choose), 1, change);
+        break;
         case 2:
           input.replace(input.find(choose), 2, change);
+        break;
         default:
-          cout << "You can't do that!" << endl << endl;
+          cout << "------------\nYou can't do that!";
           goto INPUT;
+        break;
       }
     file.open("../Configs/ConfigChanger.txt",  ofstream::out | ofstream::trunc);  // deletes contents of a file, writes new ones
     file << input << "\n1";
