98b893b6df48bab20e350f67b155aaad7fa4b26e
odka <63544489+o-dka@users.noreply.github.com>
2021-09-02 16:02:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,13 @@
 echo "Checking if gcc,cmake and ninja are installed."
 ncheck=$(which ninja)
 cmcheck=$(which cmake)
-gcheck=$(which g++)
 mcheck=$(which make)
-#gcorr=/usr/bin/g++
-if [ "$gcheck" = "/usr/bin/g++" ]; then
-	echo  "\nG++ found ,checking cmake." 
-else
-	echo -n "gcc not found, install build-essentials"
-fi
+
 if [ "$cmcheck" = "/usr/bin/cmake" ]; then
 	echo  "\nCmake found ,checking ninja."
 else
 	echo -n "Cmake not found, install it!"
+        return 1
 fi
 if [ "$ncheck" = "/usr/bin/ninja" ]; then
 	echo  "\nNinja found  starting compilation ..."
@@ -21,6 +16,6 @@ if [ "$ncheck" = "/usr/bin/ninja" ]; then
 	echo "Check /build direcotory on Menu_V_Eight file, and launch it!"
 
 else 
-	echo"\n It appears that our end user didn't read the git repo, too bad, install ninja!!"
-
+	echo"\n It appears that our end user didn't read the git repo, too bad, install ninja!!""
+        return 2
 fi
