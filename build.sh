fc548116786522b33cd6c0e5e1b429b43a82ca7e
odka <63544489+o-dka@users.noreply.github.com>
2021-09-05 00:01:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,18 +3,23 @@ echo "Checking if cmake and ninja are installed."
 ncheck=$(which ninja)
 cmcheck=$(which cmake)
 
-if [ "$cmcheck" = "/usr/bin/cmake" ]; then
-        echo  "\nCmake found ,checking ninja."
+if [ "$cmcheck" = "/usr/bin/cmake"  ] || [ "$cmcheck" ="/bin/cmake" ]; then
+      echo  "\nCmake found ,checking ninja."
 else
-        echo -n "Cmake not found, install it!"
-        break
+      echo -n "Cmake not found, install it!"
+      break
 fi
-if [ "$ncheck" = "/usr/bin/ninja" ]; then
-        echo  "\nNinja found  starting compilation ..."
-        cmake -G Ninja -S Menu/ -B build/ && cd build && ninja
-        echo "Check /build direcotory on Menu_V_Eight file, and launch it!"
+if [ "$ncheck" = "/usr/bin/ninja" ] || [ "$ncheck" = "/bin/ninja" ]; then
+      echo  "\nNinja found  starting compilation ..."
+      cmake -G Ninja -S Menu/ -B build/ && cd build && ninja
+      echo "Check /build direcotory on Menu_V_Eight file, and launch it!"
 
 else
-        echo -n "Ninja not found, install it!"
-        break
+       echo -n "Ninja not found, install it!"
+     break
 fi
+~
+~
+~
+~
+~
