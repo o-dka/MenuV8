175762c1fdcd581c882aa82c11f5bb36fedf7b7f
odka <63544489+o-dka@users.noreply.github.com>
2021-09-04 19:52:06 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ if [ "$cmcheck" = "/usr/bin/cmake" ]; then
 	echo  "\nCmake found ,checking ninja."
 else
 	echo -n "Cmake not found, install it!"
-        return 1
+        break
 fi
 if [ "$ncheck" = "/usr/bin/ninja" ]; then
 	echo  "\nNinja found  starting compilation ..."
-	 cmake -G Ninja -S Menu/ -B build/ && cd build && ninja
+	cmake -G Ninja -S Menu/ -B build/ && cd build && ninja
 	echo "Check /build direcotory on Menu_V_Eight file, and launch it!"
 
 else 
-	echo "\n It appears that our end user didn't read the git repo, too bad, install ninja!!""
-        return 1
+	echo "\n Ninja not found, install it!""
+        break
 fi
