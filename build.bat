d6f73aeb80730ffcb3990461c84f32a89f705aa1
odka <63544489+o-dka@users.noreply.github.com>
2021-10-05 20:56:54 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,9 @@
 @echo off
-title "Building MenuV8"
-cd "%cd%\MenuV8\"
-cmake  -G "Visual Studio 16 2019" -S "%cd%/Menu" -B "%cd%/MenuV8/build"
+title "MenuV8"
+cmake  -G "Visual Studio 16 2019" -S Menu -B build
 :: change the above Visual Studio version number to the one you have installed(i.e 16 2019 -> 15 2017)
-cd  "%cd%/build/" && cmake --build . --config Release 
-xcopy  %cd%/build/Info cd%/build/Release/ && xcopy %cd%/build/Changer.txt  %cd%/build/Release/
+cd  build && cmake --build . --config Release 
+xcopy  Info  Release && xcopy Changer.txt  Release
 cd Release
-echo "path to exe:" "%cd%" "\MenuV8\build\Release\Menu_V_Eigth.exe"
+echo "path to exe:" "%cd%\Menu_V_Eigth.exe"
 PAUSE
